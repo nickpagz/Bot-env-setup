cfadfc60e691d39630b91cd36f0a7782319c9be3
Nicholas Pagazani <nickp@Nicholass-MacBook-Pro-3.local>
2022-01-09 09:36:43 -0500

    clean up2

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@
 # Installing necessary packages.
 #########################################
 
-# Install latest pip
-#sudo apt-get install python3-pip
+
+# Update
+sudo apt-get update
+
+# Install Build Essentials
+sudo apt-get install build-essentials
 
 # Install TA-Lib
 sudo wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
@@ -15,20 +19,23 @@ cd ta-lib/
 make
 sudo make install
 
-# Upgrade python setuptools
-#pip3 install --upgrade setuptools
-
-# Install ta-lib wrapper from mrjbq7
-#pip3 install TA-lib
-
-# Install pandas_ta
-#pip3 install pandas_ta
-
-# Install ccxt Library
-#pip3 install ccxt
-
-# Install Py3cw Library
-#pip3 install py3cw
-
 # Install tmux
 sudo apt-get install tmux
+
+# Install latest pip
+sudo apt-get install python3-pip
+
+# Upgrade python setuptools
+pip3 install --upgrade setuptools
+
+# Install ta-lib wrapper from mrjbq7
+pip3 install TA-lib
+
+# Install pandas_ta
+pip3 install pandas_ta
+
+# Install ccxt Library
+pip3 install ccxt
+
+# Install Py3cw Library
+pip3 install py3cw
